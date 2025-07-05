a0fd83e2ba56e0acfa444b13f583d4c792132bd5
moonshine1004 <moonshine1004@naver.com>
2025-07-05 18:02:31 +0900

    250705 laptop

--- Assets/Scripts/Cards/CardPooling.cs
+++ Assets/Scripts/Cards/CardPooling.cs
@@ -3,8 +3,39 @@ using UnityEngine.Pool;
 
 public class CardPooling : MonoBehaviour
 {
-    //오브젝트 풀 선언
-    private ObjectPool<GameObject> _Cards;
+    public GameObject cardPrefab;
+    public Transform cardParent; // Canvas 하위 빈 오브젝트 (예: CardPanel)
 
-    
+    private ObjectPool<CardPrefab> cardPool;
+
+    private void Awake()
+    {
+        cardPool = new ObjectPool<CardPrefab>(
+            createFunc: () => {
+                GameObject go = Instantiate(cardPrefab, cardParent);
+                return go.GetComponent<CardPrefab>();
+            },
+            actionOnGet: card => card.gameObject.SetActive(true),
+            actionOnRelease: card => {
+                card.ResetView();
+                card.gameObject.SetActive(false);
+            },
+            actionOnDestroy: card => Destroy(card.gameObject),
+            collectionCheck: false,
+            defaultCapacity: 10,
+            maxSize: 100
+        );
+    }
+
+    public CardPrefab GetCard(Card cardData, Sprite image)
+    {
+        var card = cardPool.Get();
+        card.Initialize(cardData);
+        return card;
+    }
+
+    public void ReleaseCard(CardPrefab card)
+    {
+        cardPool.Release(card);
+    }
 }
--- Assets/Scripts/Cards/CardPrefab.cs
+++ Assets/Scripts/Cards/CardPrefab.cs
@@ -1,18 +1,45 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CardPrefab : MonoBehaviour
 {
-    public Text idText;
-    public Text costText;
-    public Text damageText;
-    public Image elementIcon;
+    [SerializeField] private TMP_Text _costText;
+    [SerializeField] private TMP_Text _damageText;
+    [SerializeField] private Image _image;
+    [SerializeField] private Image elementIcon;
 
-    private Card _card;
+    private Card cardData;
 
-    public void Setup(Card data)
+    public void Initialize(Card card)
     {
-        _card = data;
+        cardData = card;
 
+        _costText.text = card._Cost.ToString();
+        _damageText.text = card._Damage.ToString();
+
+        // 예시: 속성에 따른 색상 처리
+        //elementIcon.color = GetColorByElement(card._element);
+    }
+
+    public void ResetView()
+    {
+        cardData = null;
+        _costText.text = "";
+        _damageText.text = "";
+        //elementIcon.color = Color.white;
+    }
+
+    private Color GetColorByElement(Enum.Element element)
+    {
+        switch (element)
+        {
+            case Enum.Element.Fire: return Color.red;
+            case Enum.Element.Ice: return Color.cyan;
+            case Enum.Element.Wind: return Color.green;
+            case Enum.Element.Rock: return new Color(0.6f, 0.4f, 0.2f);
+            case Enum.Element.Elect: return Color.yellow;
+            default: return Color.gray;
+        }
     }
 }
